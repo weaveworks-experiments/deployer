1336d513d92438eaf0820840451edff458f6e649
Jonathan Lange <jml@mumak.net>
2017-02-23 15:23:38 +0000

    Hello world, iron

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+
 fn main() {
-    println!("Hello, world!");
+    fn hello_world(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, "Hello World!")))
+    }
+
+    let _server = Iron::new(hello_world).http("localhost:3000").unwrap();
+    println!("On 3000");
 }
